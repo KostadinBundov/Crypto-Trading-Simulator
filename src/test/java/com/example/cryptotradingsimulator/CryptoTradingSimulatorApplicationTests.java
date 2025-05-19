23561934f3ebdfa180d2b58aaf07266790e7e088
Kostadin Bundov <Kostadinbundov@gmail.com>
2025-05-19 11:26:39 +0300

    Initial commit

---
+++ src/test/java/com/example/cryptotradingsimulator/CryptoTradingSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptotradingsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
